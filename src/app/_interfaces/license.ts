ca04f26e917015cbd2889cbe07ef08cf8bfa10f9
Mazen Al-Asali <mazenalasali@gmail.com>
2021-10-17 14:10:31 +0200

    Add files via upload

---
+++ src/app/_interfaces/license.ts
@@ -0,0 +1,6 @@
+export interface License {
+    id: string;
+    dateCreated: Date;
+    accountType: string;
+    userId?: string;
+}
---
+++ src/app/_interfaces/user-for-creation.ts
@@ -0,0 +1,5 @@
+export interface UserForCreation {
+    name: string;
+    dateOfBirth: Date;
+    address: string;
+}
